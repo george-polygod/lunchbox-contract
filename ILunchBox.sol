268f8d29ab72975fb79ee4e3bed80d9adb846d2c
George <93863428+george-polygod@users.noreply.github.com>
2022-10-02 22:52:43 +0300

    Update ILunchBox.sol

--- ILunchBox.sol
+++ ILunchBox.sol
@@ -7,7 +7,9 @@ abstract contract ILunchBox  {
       uint256 drawId;
       uint256 startDate;
       uint256 endDate;
+      uint256 endDateRequest;
       uint256 poolInital;
+      uint256 betEnd;
       uint256 poolEnd;
       uint256 result;
       uint256 pool;
@@ -27,8 +29,13 @@ abstract contract ILunchBox  {
       uint256 drawId;
     }
 
-    // function betOrder(uint256 drawId,uint256 _number,uint256 _bet) external payable virtual;
-    // function checkMatchresult(uint256 _drawId) external payable virtual;
+    function betOrder(uint256 _number,uint256 _bet) external payable virtual;
+    function checkDrawresult(uint256 _drawId) external payable virtual;
+    function claimReward(uint256 _drawId,address account)  public virtual;
+    function earned(address account,uint256 _drawId) public view virtual returns (uint256);
+    function userOrder(address account,uint256 _drawId) public view virtual returns (uint256 number,uint256 bet);
+    function pool(uint256 _drawId,uint256 _burn) public view virtual returns (uint256);
+    function payout(uint256 _drawId,uint256 _burn) public view virtual returns (uint256 reward);
 
 
 }
